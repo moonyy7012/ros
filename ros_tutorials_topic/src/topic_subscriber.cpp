1f025dadd14658c726af7c1f02588e8124f71b78
moonyy7012 <moonyy7012@naver.com>
2020-06-23 14:22:09 +0900

    Add files via upload

---
+++ ros_tutorials_topic/src/topic_subscriber.cpp
@@ -0,0 +1,17 @@
+#include "ros/ros.h"
+#include "ros_tutorials_topic/MsgTutorial.h"
+
+void msgCallback(const ros_tutorials_topic::MsgTutorial::ConstPtr& msg){
+    ROS_INFO("recieve msg = %d", msg->stamp.sec);
+    ROS_INFO("recieve msg = %d", msg->stamp.nsec);
+    ROS_INFO("recieve msg = %d", msg->data);
+}
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "topic_subscriber");
+    ros::NodeHandle nh;
+    ros::Subscriber ros_tutorial_sub =
+        nh.subscribe("ros_tutorial_msg", 100, msgCallback);
+
+    ros::spin();
+}
